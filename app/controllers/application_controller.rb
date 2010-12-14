8b62a31a3e110d20ed438f822c833354e64e3141
Kadvin <me@kadvin.com>
2010-12-14 18:57:13 +0800

    adjust demo and views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+  MenuLoader.init(:type => :yaml) if not MenuLoader.loaded?
   customize(:all) do |ctx|
     #ctx.skin = 'basic' # default
     ctx.menu = :global_menu
--- config/initializers/app_frame_init.rb
+++ config/initializers/app_frame_init.rb
@@ -1,2 +1,3 @@
+Rails.logger.info("Init the Menu Loader...")
 I18n.locale = 'zh-cn' # I18n.default_locale doest not effect
 MenuLoader.init(:type => :yaml)
