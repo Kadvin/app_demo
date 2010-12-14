5ca619ba31350d3cd4d668b826ddbd5c2804cfaa
Kadvin <me@kadvin.com>
2010-12-14 21:35:40 +0800

    refine demo app

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   MenuLoader.init(:type => :yaml) if not MenuLoader.loaded?
   customize(:all) do |ctx|
-    #ctx.skin = 'basic' # default
+    ctx.skin = 'basic' # default
     ctx.menu = :global_menu
   end
 end
--- app/controllers/demo_controller.rb
+++ app/controllers/demo_controller.rb
@@ -2,13 +2,13 @@ class DemoController < ApplicationController
 
   customize(:all) do |ctx|
     ctx.selected_menu = :staff_menu
+    ctx.side_bar = :staff_bar
   end
 
   customize(:index) do |ctx|
     # You can specify a side bar name as side_bar for 
     #  the view component(sidebar) placed on the page's left position
-    ctx.side_bar = :staff_bar
-    ctx.structure.main.left.side_bar = :staff_bar
+    ctx.structure.main.left = :side_bar
     #ctx.structure.header.right = 'welcome'
     # or it will use the global side bar in current app context
 
@@ -16,6 +16,23 @@ class DemoController < ApplicationController
     #ctx.structure.main.right.help   = :help_group
   end
 
-  def index
+  customize(:new, :edit) do |ctx|
+    ctx.side_bar = :admin_bar
+    ctx.struct.main.left = nil
+    ctx.struct.main.right = :side_bar
   end
+
+  customize(:edit){|ctx| ctx.struct.main.top = ctx.struct.main.bottom = nil}
+
+  customize(:show) do |ctx|
+    ctx.struct.main.left = :side_bar
+    ctx.struct.main.left.locals.side_bar = :leader_bar
+    ctx.struct.main.right = :help_bar
+    ctx.struct.main.right.locals.help = :help_group
+  end
+
+  def index; end
+  def new; end
+  def edit; end
+  def show; end
 end
