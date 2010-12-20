daf0269786ec25babcac68ac51fe9bb0485440e5
Kadvin <me@kadvin.com>
2010-12-20 13:11:55 +0800

    make paginate table skin work

--- app/controllers/app_table_demo_controller.rb
+++ app/controllers/app_table_demo_controller.rb
@@ -6,18 +6,24 @@ class AppTableDemoController < ApplicationController
     ctx.selected_menu = :staff_menu
     ctx.side_bar = :staff_bar
   end
+
   #
   # == Browse topics
   # * Frame: GET /app_table_demo/topics
   # * Page:  GET /app_table_demo/topics_page
   browse_as_table :topics, :skin => :endless
-  customize(:topics){|ctx| ctx.javascripts << 'endless'}
-
-  def new
-
+  customize(:topics) do |ctx|
+    ctx.javascripts << 'endless'
+    ctx.stylesheets << 'endless'
   end
 
-  def create
-    
+  #
+  # == Browse posts
+  # * Frame: GET /app_table_demo/posts
+  # * Page:  GET /app_table_demo/posts_page
+  browse_as_table :posts, :skin => :paginate
+  customize(:posts) do |ctx|
+    ctx.javascripts << 'paginate'
+    ctx.stylesheets << 'paginate'
   end
 end
