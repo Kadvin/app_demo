e29721fb9322fb857574531b738cdfad76b16cb9
Kadvin <me@kadvin.com>
2010-12-24 10:27:23 +0800

    app table work with acts as enum groupby

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -7,4 +7,6 @@ class Topic < ActiveRecord::Base
   has_many :posts, :dependent => :destroy
   has_many :topic_tags, :dependent => :destroy
   has_many :tags, :through => :topic_tags
+
+  acts_as_enum :state, :new, :read, :approved
 end
